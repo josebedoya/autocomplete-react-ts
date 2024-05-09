b904140106b32d91677f75d055fb1dd51d777572
Jose Bedoya <jossephb@gmail.com>
2024-05-09 08:18:17 -0500

    call an endpoint and set the data in a useState

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './Autocomplete.styles.css'
+import { type Post } from '../../types'
 
 const Autocomplete = () => {
   const [searchValue, setSearchValue] = useState('')
+  const inputRef = useRef<HTMLInputElement | null>(null)
+  const [data, setData] = useState<Post[]>([])
+  const [error, setError] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  useEffect(() => {
+    const fetchData = async() => {
+      setError(null)
+      setIsLoading(true)
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const result = await response.json() as Post[]
+        setData(result)
+      } catch (error: unknown) {
+        setError('Something went wrong')
+        setIsLoading(false)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
       <h1>Autocomplete</h1>
       <div className="autocomplete">
         <input
           type="text"
+          ref={inputRef}
           placeholder="Type something..."
           value={searchValue}
           onChange={(event) => setSearchValue(event.target.value)}
